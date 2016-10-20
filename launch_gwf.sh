6726e7a33e406b7f8cd26fcf2466d9b7d113a260
Amanda Calatrava <acalatrava46@gmail.com>
2016-10-20 19:46:18 +0200

    Fix some bugs

--- launch_gwf.sh
+++ launch_gwf.sh
@@ -1,21 +1,21 @@
-
+#!/bin/bash
 t0="$( date +%s )"
 mintime="-1"
 echo "starting execution!" $(date) >> /home/ubuntu/launch_gwf.out
 cat $1 | while read a t a r n a; do
-	[ $(( mintime < 0 )) -eq 1 ] && mintime="$t"
-	while true; do
-		t1="$( date +%s )"
-		echo $t $mintime $t1 $t0
-		if [ $(( t-mintime <= t1-t0 )) -eq 1 ]; then
-			for ((i=0; i<n; i++)); do
-				# Remplaza por un comando que lanza un sleep de $r secs
-				echo sbatch script.slurm $r
-				sbatch script.slurm $r &
-			done
-			break
-		fi
-		sleep 1
-	done
+    [ $(( mintime < 0 )) -eq 1 ] && mintime="$t"
+    while true; do
+        t1="$( date +%s )"
+        echo $t $mintime $t1 $t0
+        if [ $(( t-mintime <= t1-t0 )) -eq 1 ]; then
+            for ((i=0; i<n; i++)); do
+                # Remplaza por un comando que lanza un sleep de $r secs
+                echo sbatch script.slurm $r
+                sbatch script.slurm &
+            done
+            break
+        fi
+        sleep 1
+    done
 done
 echo "Last job launched:" $(date) >> /home/ubuntu/launch_gwf.out
